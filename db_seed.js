7c773a0151c113e932c54093cad18da82d5c5aac
David Wimmel <wimmeldj@miamioh.edu>
2019-04-13 13:52:04 -0400

    db_seed.js is finalized.   For users: populates an admin, mod, and two test users with profile pictures.   For applications: five applications.   For pre_applications: One pre_application which is a clone of an already existing application.   For comments: random comments made by various users across the five applications.
    init.sh is finalized.
      It clears all user uploaded media from ./public
      Runs the db_seed script
      Prints database response for queries finding testadmin, testmod, testuser1, and testuser2
    
    TODO:
      Write test scripts and call them with init.sh. One should test api responses.

--- db_seed.js
+++ db_seed.js
@@ -1,137 +1,233 @@
 'use strict';
-const MongoClient = require('mongodb').MongoClient;
-const bcrypt = require('bcrypt');
-const assert = require('assert');
-const {MONGODB_URL} = require('./config');
+const mongoose = require('mongoose')
+const assert = require('assert')
+const bcrypt = require('bcrypt')
+const fs = require('fs')
+const {hashPassword, comparePassword} = require('./server/utils/database')
+const {Application, PreApplication, Comment, User} = require('./server/models')
 
-const client = new MongoClient(MONGODB_URL, {useNewUrlParser: true});
+function assoc_with_image (from_path, to_path, obj) {
+  let id = obj._id
+  fs.copyFile(from_path, to_path + id, (err) => {
+    if (err) throw err
+  })
+}
 
-// Creates database, collections, and default entries used in application
-client.connect( (err) => {
-  assert.equal(null, err);
-  console.log("Successful connection with mongodb");
+// users seeding
+let admin_user = new User({
+  fname: 'test',
+  lname: 'admin',
+  uname: 'testadmin',
+  pass: bcrypt.hashSync('pass', 11),
+  group: 'admin',
+  media_path: null
+})
+admin_user.save().then(() => {
+  console.log("Created testadmin: ", admin_user)
+})
 
-  const db = client.db("vrepo_db");
-  // 3 test users with different privilege levels
-  db.collection("users").insertMany([
-    {
-      fname: 'alfred',
-      lname: 'admin',
-      uname: 'alfredadmin',
-      group: 'admin',
-      pass: bcrypt.hashSync('admin pass', 11),
-      media_path: null
-    },
-    {
-      fname: 'martha',
-      lname: 'mod',
-      uname: 'marthamod',
-      group: 'mod',
-      pass: bcrypt.hashSync('mod pass', 11),
-      media_path: null
-    },
-    {
-      fname: 'david',
-      lname: 'wimmel',
-      uname: 'davidwimmel',
-      group:  'user',
-      pass: bcrypt.hashSync('david pass', 11),
-      media_path: null
-    }
-  ], (err, result) => {
-    assert.equal(null, err);
-    console.log("Inserted users");
-  });
+let mod_user = new User({
+  fname: 'test',
+  lname: 'mod',
+  uname: 'testmod',
+  pass: bcrypt.hashSync('pass', 11),
+  group: 'mod',
+  media_path: null
+})
+mod_user.save().then( () => {
+  console.log("Created testmod: ", mod_user)
+})
 
-  // TODO: may want to change media_path to a field which stores a BSON document or BinData (or something else) for storing .jpg files
-  // TODO: this will allow users to submit images rather than relying on manually adding images to some '/images' dir
-  // 3 video game applications with different fields
-  db.collection("applications").insertMany([
-    {
-      title: 'test_app1',
-      desc:  'test_app1 description. This is a description for test_app1',
-      genre: 'fps',
-      platform: ['linux', 'macos', 'windows'],
-      developer: 'app1_dev',
-      publisher: 'app1_pub',
-      release: new Date(2018, 1, 1),
-      rating: 'E',
-      tags: ['tag1', 'tag2', 'tag3', 'test_app1'],
-      version: 1.2,
-      price: 12.99,
-      hyperlink: 'test_app1_url',
-      media_path: '/'
-    },
-    {
-      title: 'test_app2',
-      desc:  'test_app2 description. This is a description for test_app2',
-      genre: 'rpg',
-      platform: ['linux'],
-      developer: 'app2_dev',
-      publisher: 'app2_pub',
-      release: new Date(2019, 12, 12),
-      rating: 'T',
-      tags: ['tag1', 'tag2', 'tag3', 'test_app2'],
-      version: 0.4,
-      price: 9.99,
-      hyperlink: 'test_app2_url',
-      media_path: '/'
-    },
-    {
-      title: 'hotline miami',
-      desc:  "Hotline Miami is a high-octane action game overflowing with raw brutality, hard-boiled gunplay and skull crushing close combat.",
-      genre: 'action',
-      platform: ['linux', 'macos', 'windows'],
-      developer: "Dennaton Games",
-      publisher: "Devolver Digital",
-      release: new Date(2012, 10, 23),
-      rating: 'M',
-      tags: ['great soundtrack', 'action', 'indie', 'psychedelic', 'difficult',
-       'Top-Down Shooter'],
-      version: 1.2,
-      price: 12.99,
-      hyperlink: 'test_app1_url',
-      media_path: '/'
-    },
-  ], (err, result) => {
-    assert.equal(null, err);
-    console.log("Inserted applications");
-  });
+let lower_user = new User({
+  fname: 'test',
+  lname: 'user',
+  uname: 'testuser1',
+  pass: bcrypt.hashSync('pass', 11),
+  group: 'user',
+  media_path: '/public/users/'
+})
+lower_user.save().then( () => {
+  assoc_with_image('./db_seed_assets/test1.png', './public/users/', lower_user)
+  console.log("Created testuser: ", lower_user)
+})
+
+let lower_user2 = new User({
+  fname: 'test',
+  lname: 'user',
+  uname: 'testuser2',
+  pass: bcrypt.hashSync('pass', 11),
+  group: 'user',
+  media_path: '/public/users/'
+})
+lower_user2.save().then( () => {
+  assoc_with_image('./db_seed_assets/test2.gif', './public/users/', lower_user2)
+  console.log("Created testuser2: ",  lower_user)
+})
+
+// used to seed many users which will be used for comment seeding
+let lower_users = []
+for (let i = 3; i < 23; i++) {
+  let j = lower_users.push(
+    new User({
+      fname: 'test',
+      lname: 'user',
+      uname: `testuser${Math.floor(Math.random() * 10000000)}`,
+      pass: bcrypt.hashSync('pass', 11),
+      group: 'user'
+    })
+  )
+  lower_users[j-1].save()
+}
 
 
-  db.collection("comments").insertMany([
-    {
-      user_id: 'xxx',
-      app_id: 'xxx',
-      comment: "This is a test comment by David Wimmel for the game Hotline Miami. Is it displaying? Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    }
-  ], (err, result) => {
-    assert.equal(null, err);
-    console.log("Inserted comments");
-  });
+// application seeding
+let app1 = new Application({
+  title: 'hotline miami',
+  desc: "Hotline Miami is a high-octane action game overflowing with raw brutality, hard-boiled gunplay and skull crushing close combat. Set in an alternative 1989 Miami, you will assume the role of a mysterious antihero on a murderous rampage against the shady underworld at the behest of voices on your answering machine. Soon you'll find yourself struggling to get a grip of what is going on and why you are prone to these acts of violence. Rely on your wits to choreograph your way through seemingly impossible situations as you constantly find yourself outnumbered by vicious enemies. The action is unrelenting and every shot is deadly so each move must be quick and decisive if you hope to survive and unveil the sinister forces driving the bloodshed. Hotline Miami’s unmistakable visual style, a driving soundtrack, and a surreal chain of events will have you question your own thirst for blood while pushing you to the limits with a brutally unforgiving challenge.",
+  genre: 'action',
+  platform: ['linux', 'macos', 'windows'],
+  developer: 'dennaton games',
+  publisher: 'devolver digital',
+  release: new Date('01/01/10'),
+  rating: 'm',
+  tags: ['psychedelic', 'violent', 'indie', 'action'],
+  version: '1.0',
+  price: '9.99',
+  hyperlink: 'https://store.steampowered.com/app/219150/Hotline_Miami/',
+  dateAdded: new Date(),
+  media_path: null
+})
+app1.save().then( () => {
+  assoc_with_image('./db_seed_assets/hotline.jpg', './public/media/', app1)
+  console.log("Created app1: ", app1)
+})
 
-  db.collection("pre_applications").insertMany([
-    {
-      title: 'submitted_app',
-      desc: 'desc for submitted_app',
-      genre: 'some genre',
-      platform: ['linux'],
-      developer: 'some developer',
-      publisher: 'some publisher',
-      release: new Date(2019, 2, 3),
-      rating: 'E',
-      tags: ['tag1', 'tag2'],
-      version: 1.0,
-      price: 10.99,
-      hyperlink: 'some link',
-      date_added: new Date(),
-      media_path: '/'
-    }
-  ], (err, result) => {
-    assert.equal(null, err);
-    console.log('Inserted pre-applicaitons');
-  });
+let app2 = new Application({
+  title: 'fistful of frags',
+  desc: "Fistful of Frags was born years ago as a Wild West themed modification for Source engine. It has been completely renewed for its Steam release, paying special attention to combat mechanics. Also please note this is a completely *free* standalone mod, no micro-transactions exist, no registration required. Just install and play. You may see ads when joining certain third party servers that host our game for free. That's however completely unrelated to FoF dev team, we do not profit from them.",
+  genre: 'fps',
+  platform: ['linux', 'windows', 'macos'],
+  developer: 'fistful of frags team',
+  publisher: 'fistful of frags team',
+  release: new Date('01/02/10'),
+  rating: 'nr',
+  tags: ['free to play', 'multiplayer', 'fps', 'western', 'action'],
+  version: '1.0',
+  price: 0.01,
+  hyperlink: 'https://store.steampowered.com/app/265630/Fistful_of_Frags/',
+  dateAdded: new Date(),
+  media_path: null
+})
+app2.save().then( () => {
+  assoc_with_image('./db_seed_assets/fof.jpg', './public/media/', app2)
+  console.log("Created app2: ", app2)
+})
 
-  console.log("===done");
-  client.close();
-});
+let app3 = new Application({
+  title: 'risk of rain 2',
+  desc: "The classic multiplayer roguelike, Risk of Rain, returns with an extra dimension and more challenging action. Play solo, or team up with up to three friends to fight your way through hordes of monsters, unlock new loot, and find a way to escape the planet.",
+  genre: 'action',
+  platform: ['windows'],
+  developer: 'hopoo games',
+  publisher: 'gearbox publishing',
+  release: new Date('01/03/10'),
+  rating: 'nr',
+  tags: ['early access', 'rogue-like', 'mutliplayer', 'co-op'],
+  version: '1.0',
+  price: '19.99',
+  hyperlink: 'https://store.steampowered.com/app/632360/Risk_of_Rain_2/',
+  dateAdded: new Date(),
+  media_path: null
+})
+app3.save().then( () => {
+  assoc_with_image('./db_seed_assets/ror.jpg', './public/media/', app3)
+  console.log("Created app3: ", app3)
+})
+
+let app4 = new Application({
+  title: 'terraria',
+  desc: "Dig, fight, explore, build! Nothing is impossible in this action-packed adventure game. Four Pack also available!",
+  genre: 'action',
+  platform: ['windows', 'macos', 'linux'],
+  developer: 're-logic',
+  publisher: 're-logic',
+  release: new Date('01/04/10'),
+  rating: 'nr',
+  tags: ['sandbox', 'adventure', 'survival', '2D', 'crafting'],
+  version: '1.0',
+  price: '9.99',
+  hyperlink: 'https://store.steampowered.com/app/105600/Terraria/',
+  dateAdded: new Date(),
+  media_path: null
+})
+app4.save().then( () => {
+  assoc_with_image('./db_seed_assets/terr.jpg', './public/media/', app4)
+  console.log("Created app4: ", app4)
+})
+
+let app5 = new Application({
+  title: 'rimworld',
+  desc: "A sci-fi colony sim driven by an intelligent AI storyteller. Inspired by Dwarf Fortress and Firefly. Generates stories by simulating psychology, ecology, gunplay, melee combat, climate, biomes, diplomacy, interpersonal relationships, art, medicine, trade, and more.",
+  genre: 'strategy',
+  platform: ['windows', 'macos', 'linux'],
+  developer: 'ludeon studios',
+  publisher: 'ludeon studios',
+  release: new Date('01/05/10'),
+  rating: 'nr',
+  tags: ['base building', 'survival', 'strategy', 'sandbox', '2D'],
+  version: '1.0',
+  price: '34.99',
+  hyperlink: 'https://store.steampowered.com/app/294100/RimWorld/',
+  dateAdded: new Date(),
+  media_path: null
+})
+app5.save().then( () => {
+  assoc_with_image('./db_seed_assets/rim.jpg', './public/media/', app5)
+  console.log("Created app5: ", app5)
+})
+
+
+// comments seeding with randomly selected users
+let apps = [app1, app2, app3, app4, app5]
+lower_users.forEach( (user) => {
+  if (Math.random() < 0.75) {
+    apps.forEach( (app) => {
+      if (Math.random() < 0.75) {
+        let comment = new Comment({
+          user_id: user._id,
+          app_id: app._id,
+          uname: user.uname,
+          user_media_path: user.media_path,
+          comment: `This is a test comment. This comment was made by ${user.uname} with id ${user._id} on application ${app.title}`
+        })
+        comment.save().then( () => {
+          console.log(`Comment made by ${user.uname} on ${app.title}`)
+        })
+      }
+    })
+  }
+})
+
+
+// pre_applications seeding
+let papp1 = new PreApplication({
+  title: 'rimworld',
+  desc: "A sci-fi colony sim driven by an intelligent AI storyteller. Inspired by Dwarf Fortress and Firefly. Generates stories by simulating psychology, ecology, gunplay, melee combat, climate, biomes, diplomacy, interpersonal relationships, art, medicine, trade, and more.",
+  genre: 'strategy',
+  platform: ['windows', 'macos', 'linux'],
+  developer: 'ludeon studios',
+  publisher: 'ludeon studios',
+  release: new Date('01/05/10'),
+  rating: 'nr',
+  tags: ['base building', 'survival', 'strategy', 'sandbox', '2D'],
+  version: '1.0',
+  price: '34.99',
+  hyperlink: 'https://store.steampowered.com/app/294100/RimWorld/',
+  dateAdded: new Date(),
+  media_path: null
+})
+papp1.save().then( () => {
+  assoc_with_image('./db_seed_assets/rim.jpg', './public/media/uploads/', papp1)
+  console.log("Created pre_app1: ", papp1)
+  mongoose.connection.close()
+})
--- init.sh
+++ init.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
-echo -e "\e[32mEnsure a mongodb server is running in local project: mongod --dbpath=.data/db --port 27018\e[31m"
+echo -e "\e[91mRemoving all images in /public/media and /public/users"
+find ./public/media/ -type f -not -name '*.git*' | xargs rm
+find ./public/users/ -type f -not -name '*.git*' | xargs rm
+
+echo -e "\e[32mEnsure a mongodb server is running in local project: \e[97mmongod \
+--dbpath=.data/db --port 27018\e[39m"
 mongo localhost:27018/vrepo_db --eval "db.dropDatabase();"
 node db_seed.js
 
-mongo localhost:27018/vrepo_db --eval "db.users.findOne({ uname: 'davidwimmel' })._id"
-mongo localhost:27018/vrepo_db --eval "db.applications.findOne({ title: 'hotline miami' })._id"
+echo -e "\e[96mPassword for all seeded users is: \e[92m'pass'\e[96m.\n\
+Useful usernames are: \e[92mtestadmin, testmod, testuser1, testuser2"
+echo -e "\e[97m"
+mongo localhost:27018/vrepo_db --eval "db.users.find({ uname: {\$in: \
+['testadmin', 'testmod', 'testuser1', 'testuser2']} }).pretty()"
 
-echo -e "\e[32mEnd"
-
+echo -e "\e[32mEnd Database Seeding"
