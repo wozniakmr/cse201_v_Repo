66c1a76d529cc8a1661619f2c7b16f8058239cce
David Wimmel <wimmeldj@miamioh.edu>
2019-04-22 14:59:56 -0400

    refactor sample_test.sh

--- sample_test.sh
+++ sample_test.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+HOST="localhost"
+PORT=3000
 
 # colors
 green=$"\e[92m"
@@ -7,8 +9,6 @@ yellow=$"\e[93m"
 color_off=$"\e[0m"
 
 # TEST 1 - check for redirect to /applications when requesting $HOST:$PORT
-HOST="localhost"
-PORT=3000
 RES=$(curl -s http://$HOST:$PORT)
 echo -e "${yellow}GET $HOST:$PORT ${color_off}"
 if echo $RES | grep -i -q 'Found. Redirecting to /applications'; then
